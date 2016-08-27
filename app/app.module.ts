512aa7521ddc4c30ca166d206f2af7c18ba8eaac
Dhaval Patel <mail@dhavalpatel.co.uk>
2016-08-27 14:02:43 +0100

    added contacts module. The main app module is now less busy and contacts is seperated.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,26 +1,18 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
-import { HighlightDirective }  from './highlight.directive';
-import { TitleComponent }  from './title.component';
-import {FormsModule} from '@angular/forms';
-import {AwesomePipe} from './contact/awesome.pipe';
-import {ContactComponent} from './contact/contact.component';
-import { HighlightDirective as ContactHighlightDirective} from './contact/highlight.directive';
-import {ContactService} from './contact/contact.service';
-import {UserService} from './user.service';
-
+import { NgModule }           from '@angular/core';
+import { BrowserModule }      from '@angular/platform-browser';
+/* App Root */
+import
+       { AppComponent }       from './app.component';
+import { HighlightDirective } from './highlight.directive';
+import { TitleComponent }     from './title.component';
+import { UserService }        from './user.service';
+/* Contact Imports */
+import
+       { ContactModule }      from './contact/contact.module';
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HighlightDirective,
-    TitleComponent,
-    AwesomePipe,
-    ContactComponent,
-    ContactHighlightDirective
-  ],
-  providers: [ ContactService, UserService ],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, ContactModule ],
+  declarations: [ AppComponent, HighlightDirective, TitleComponent ],
+  providers:    [ UserService ],
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
