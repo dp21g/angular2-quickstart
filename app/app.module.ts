02a305c6b46b19149d3c389f4cc07341b3f2bf4d
Dhaval Patel <mail@dhavalpatel.co.uk>
2016-09-04 01:01:34 +0100

    working routes except 1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,26 +1,24 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent }  from './app.component';
 import { HighlightDirective }  from './highlight.directive';
 import { TitleComponent }  from './title.component';
 import {UserService } from './user.service';
+
 import {FormsModule} from '@angular/forms';
-import {ContactService} from './contact/contact.service';
-import {AwesomePipe} from './contact/awesome.pipe';
-import {ContactComponent} from './contact/contact.component';
 import {HighlightDirective as ContactHighlightDirective} from './contact/highlight.directive';
+import {ContactModule} from './contact/contact.module';
+import { routing } from './app.routing';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ContactModule, routing],
   declarations: [
     AppComponent,
     HighlightDirective,
-    TitleComponent,
-    AwesomePipe,
-    ContactComponent,
-    ContactHighlightDirective
+    TitleComponent
   ],
-  providers: [ContactService, UserService ],
+  providers: [UserService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
