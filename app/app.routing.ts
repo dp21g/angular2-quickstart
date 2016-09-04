4856d387aaa6e566e3d576f5020f7f412d1a9813
Dhaval Patel <mail@dhavalpatel.co.uk>
2016-09-04 01:10:31 +0100

    all working now

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 export const routes: Routes = [
   { path: '', redirectTo: 'contact', pathMatch: 'full'},
   { path: 'crisis', loadChildren: 'app/crisis/crisis.module#CrisisModule' },
-  { path: 'heroes', loadChildren: 'app/hero/hero.module.3#HeroModule' }
+  { path: 'heroes', loadChildren: 'app/hero/hero.module#HeroModule' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
